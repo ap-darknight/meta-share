3c7c5ad59b3d7079cc4b5a6451adf86c9fa81e19
Ashutosh Pandey <pandeyashutosh150799@gmail.com>
2022-03-11 13:26:19 +0530

    Final Completion!

--- server.js
+++ server.js
@@ -4,25 +4,24 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const path = require('path');
 const cors = require('cors');
+const connectDB = require('./config/db');
 // Cors 
-const corsOptions = {
-  origin: process.env.ALLOWED_CLIENTS.split(',')
-  // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
-}
-
+// const corsOptions = {
+//   origin: process.env.ALLOWED_CLIENTS.split(',')
+//   // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
+// }
+connectDB();
 // Default configuration looks like
-// {
-//     "origin": "*",
-//     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-//     "preflightContinue": false,
-//     "optionsSuccessStatus": 204
-//   }
+const corsOptions = {
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+};
 
 app.use(cors(corsOptions))
 app.use(express.static('public'));
 
-const connectDB = require('./config/db');
-connectDB();
 
 app.use(express.json());
 
